c9228b099ee84a7d93ca261bfbc2739aaa18dbd5
Sebastian Iturregui-Shelton <Seebast4ever@users.noreply.github.com>
2014-07-14 14:39:29 -0400

    Initial Commit

---
+++ proj1/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  proj1
+//
+//  Created by Seebast4ever on 7/14/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
